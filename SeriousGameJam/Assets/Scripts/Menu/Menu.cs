5db93f0ca3e6c69258667a8b593ba0ae30e438ed
Team <timonsol13@gmail.com>
2020-12-02 22:31:40 +0200

    Menu template

---
+++ SeriousGameJam/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using NaughtyAttributes;
+using Random = UnityEngine.Random;
+
+public class Menu : MonoBehaviour {
+	[Header("Refs"), Space]
+	[SerializeField] DialogLogUI dialogLog;
+	[SerializeField] DialogSelectUI dialogSelect;
+	[SerializeField] ManualUI manual;
+	[Space]
+	[SerializeField] Level level;
+
+
+	void Start() {
+		level.OnLevelEndEvent += OnGameEnd;
+
+		StartGame();
+	}
+
+	private void OnDestroy() {
+		level.OnLevelEndEvent -= OnGameEnd;
+	}
+
+	void StartGame() {
+		level.StartGame();
+	}
+
+	void OnGameEnd() {
+
+	}
+}
